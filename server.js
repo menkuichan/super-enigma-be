f1d7fed3a536219a98c84485a2734855fc648a8a
IlonaMenkui <ilonaspolding@gmail.com>
2019-12-05 16:10:39 +0300

    add hello world after api request

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-'use strict';
-
 const express = require('express');
+const axios = require('axios');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/popular';
+
+function getHelloWorld() {
+  axios.get(BASE_URL,
+    {
+      params: {
+        api_key: 'ab7c9fc53125a8e8d9fd23c8704f80e5',
+        page: 1,
+      },
+    })
+    .then((res) => {
+      const { total_pages } = res.data;
+      console.log(`Total pages: ${total_pages}
+      Hello word!`);
+    });
+}
+
+getHelloWorld();
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send(getHelloWorld());
 });
 
 app.listen(PORT, HOST);
