231ceba8e1885588b7382199093095e045d06924
IlonaMenkui <ilonaspolding@gmail.com>
2019-12-05 18:24:38 +0300

    code refactoring

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require('http');
 const axios = require('axios');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOST || '127.0.0.1';
 const BASE_URL = 'https://api.themoviedb.org/3/movie/popular';
 const API_KEY = 'ab7c9fc53125a8e8d9fd23c8704f80e5';
 
@@ -15,10 +15,7 @@ function getTotalPages() {
         page: 1,
       },
     })
-    .then((res) => {
-      const { total_pages } = res.data;
-      return total_pages;
-    });
+    .then(({ data: { total_pages } }) => total_pages);
 }
 
 // App
@@ -28,6 +25,6 @@ http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end(`Hello World and total pages - ${total_pages}`);
   });
-}).listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}/`);
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
