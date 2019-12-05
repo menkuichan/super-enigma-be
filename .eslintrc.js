054308ff0a693c84076228b3cb3d7feb0f2bd594
IlonaMenkui <ilonaspolding@gmail.com>
2019-12-05 18:16:02 +0300

    add Hello World after api response

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es6: true,
   },
   extends: [
     'airbnb-base',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 6,
   },
   rules: {
+    "no-console": 0,
+    "camelcase": 1,
   },
 };
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express');
+const http = require('http');
 const axios = require('axios');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const BASE_URL = 'https://api.themoviedb.org/3/movie/popular';
+const API_KEY = 'ab7c9fc53125a8e8d9fd23c8704f80e5';
 
 function getHelloWorld() {
-  axios.get(BASE_URL,
+  return axios.get(BASE_URL,
     {
       params: {
-        api_key: 'ab7c9fc53125a8e8d9fd23c8704f80e5',
+        api_key: API_KEY,
         page: 1,
       },
     })
     .then((res) => {
       const { total_pages } = res.data;
-      console.log(`Total pages: ${total_pages}
-      Hello word!`);
+      return total_pages;
     });
 }
 
-getHelloWorld();
-
 // App
-const app = express();
-app.get('/', (req, res) => {
-  res.send(getHelloWorld());
+http.createServer((req, res) => {
+  getHelloWorld().then((total_pages) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello World and total pages - ${total_pages}`);
+  });
+}).listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
 });
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
