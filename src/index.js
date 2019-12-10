a067fa42a93adac8c887320cef30e3f2148670e8
IlonaMenkui <ilonaspolding@gmail.com>
2019-12-10 17:24:33 +0300

    add mongoose and movie model

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const assert = require('assert');
 const express = require('express');
 const { getPages } = require('./controllers/get');
@@ -13,6 +14,8 @@ const dbName = 'ilona';
 // Create a new MongoClient
 const client = new MongoClient(url);
 
+const { Schema } = mongoose;
+
 // Use connect method to connect to the Server
 client.connect((err) => {
   assert.equal(null, err);
@@ -21,6 +24,27 @@ client.connect((err) => {
   const db = client.db(dbName);
 
   client.close();
+});
+
+const movieScheme = new Schema({
+  title: String,
+  overview: String,
+});
+
+mongoose.connect('mongodb://localhost:27017/usersdb', { useNewUrlParser: true });
+
+const Movie = mongoose.model('Movie', movieScheme);
+
+const movie = new Movie({
+  title: 'Fight Club',
+  overview: 'Best movie',
+});
+
+movie.save((err) => {
+  mongoose.disconnect();
+  if (err) return console.log(err);
+
+  console.log('Saved object:', movie);
 });
 
 const app = express();
