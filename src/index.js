7766de071e4c6af688ce02a383aa5bea42713a92
IlonaMenkui <ilonaspolding@gmail.com>
2019-12-10 12:58:24 +0300

    add mongodb into the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
 const express = require('express');
 const { getPages } = require('./controllers/get');
 const { PORT, HOST } = require('./constants');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'ilona';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect((err) => {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+
+  const db = client.db(dbName);
+
+  client.close();
+});
 
 const app = express();
 app.get('/', (req, res) => {
