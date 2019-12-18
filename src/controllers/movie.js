0d5eb2cae1b16e3ef11456ca0e4fc974f69e24aa
IlonaMenkui <ilonaspolding@gmail.com>
2019-12-18 14:29:35 +0300

    code refactoring

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -3,32 +3,27 @@ const { findMovieById, deleteMovie, getMovies, createMovie, updateMovie } = requ
 exports.getMovieById = async (req, res) => {
   const { movieId } = req.params;
   const movie = await findMovieById(movieId);
-  res.setHeader('Content-Type', 'application/json');
   res.status(200).json(movie);
 };
 
 exports.createMovie = async (req, res) => {
   const movie = await createMovie(req.body);
-  res.setHeader('Content-Type', 'application/json');
   res.status(200).json(movie);
 };
 
 exports.getMovies = async (req, res) => {
   const movies = await getMovies();
-  res.setHeader('Content-Type', 'application/json');
   res.status(200).json(movies);
 };
 
 exports.deleteMovie = async (req, res) => {
   const { id } = req.params;
   const movie = await deleteMovie(id);
-  res.setHeader('Content-Type', 'application/json');
   res.status(200).json(movie);
 };
 
 exports.updateMovie = async (req, res) => {
   const { params: { id }, body } = req;
   const movie = await updateMovie(id, body);
-  res.setHeader('Content-Type', 'application/json');
   res.status(200).json(movie);
 };
